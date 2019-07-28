0f3fa39ba3b3fbf5b9cc771d6a37da832fd55c57
lixt <lixt@smartdot.com>
2019-07-28 15:18:28 +0800

    微信登陆

---
+++ pages/user/user.vue
@@ -0,0 +1,61 @@
+<template>
+    <view class="content">
+		<template v-if="hasLogin">
+			<view class="u-f user-info">
+				<image :src="avatarUrl" mode="widthFix" lazy-load></image>
+				<view class="u-f-ajc">欢迎,{{userName}}！</view>
+			</view>
+		</template>
+		
+        <view class="btn-row login-btn">
+            <button v-if="!hasLogin" type="primary" class="primary" @tap="bindLogin">登录</button>
+            <button v-if="hasLogin" type="default" @tap="bindLogout">退出登录</button>
+        </view>
+    </view>
+</template>
+
+<script>
+    import {
+        mapState,
+        mapMutations
+    } from 'vuex'
+
+    export default {
+        computed: {
+            ...mapState(['hasLogin', 'forcedLogin', 'avatarUrl', 'userName'])
+        },
+        methods: {
+            ...mapMutations(['logout']),
+            bindLogin() {
+                uni.navigateTo({
+                    url: '../login/login',
+                });
+            },
+            bindLogout() {
+                this.logout();
+                /**
+                 * 如果需要强制登录跳转回登录页面
+                 */
+                if (this.forcedLogin) {
+                    uni.reLaunch({
+                        url: '../login/login',
+                    });
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+.login-btn{
+	margin-top:60%;
+}
+.user-info{
+	padding: 20upx;
+}
+.user-info>image{
+	width: 100upx;
+	border-radius: 100%;
+	margin-right: 10upx;
+}
+</style>
---
+++ store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+        /**
+         * 是否需要强制登录
+         */
+        forcedLogin: false,
+        hasLogin: false,
+        userName: "",
+		avatarUrl:""
+    },
+    mutations: {
+        login(state, userInfo) {
+			console.log('state='+JSON.stringify(state)+' userName='+userInfo.nickName+' avatarUrl='+userInfo.avatarUrl)
+            state.userName = userInfo.nickName || '新用户';
+            state.hasLogin = true;
+			state.avatarUrl = userInfo.avatarUrl;
+        },
+        logout(state) {
+            state.userName = "";
+            state.hasLogin = false;
+        }
+    }
+})
+
+export default store
